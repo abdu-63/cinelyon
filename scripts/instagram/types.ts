69f34ca6d5f5e720f7c05a0bd46963e449105190
Abdu <abdur.palta@icloud.com>
2026-04-07 10:01:52 +0200

    Reapply "module 0"
    This reverts commit 94461dacb162bac2bc45b6872614d5fe58945e6c.

---
+++ scripts/instagram/types.ts
@@ -0,0 +1,23 @@
+export interface SelectedFilm {
+  title: string;
+  year: number;
+  director: string;
+  note: number;
+  score: number;
+  senscritique_rank: number;
+}
+
+export interface Cinema {
+  name: string;
+  address: string;
+  showtimes: string[];
+  passes: string[];
+}
+
+export interface EnrichedFilm {
+  title: string;
+  director: string;
+  year: number;
+  poster_url: string;
+  cinema: Cinema[];
+}
