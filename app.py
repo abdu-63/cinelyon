14de654e32ab169c5f8284ae0388ec882f0b9f82
abdu-63 <abdur.palta@icloud.com>
2026-04-28 13:26:59 +0200

    page 404

--- app.py
+++ app.py
@@ -331,6 +331,10 @@ def home():
         all_formats=sorted(all_formats),
     )
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", website_title=WEBSITE_TITLE), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
