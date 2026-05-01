8c593c98a24a85410426e25861d761f9741cf46d
abdu-63 <abdur.palta@icloud.com>
2026-05-01 17:06:06 +0200

    ruff fix

--- app.py
+++ app.py
@@ -331,6 +331,7 @@ def home():
         all_formats=sorted(all_formats),
     )
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html", website_title=WEBSITE_TITLE), 404
